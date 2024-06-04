0e830a91a1cd8e68712da675f8d839560d09411c
HuseinHQ <mhusein463@gmail.com>
2024-06-04 15:05:52 +0700

    feat: Add Savart logo and heading to homepage
    - Added the Savart logo to the homepage using lazy loading for improved page load performance.
    - Included a heading with a descriptive text to provide information about the product.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,19 @@
 import Navbar from '@/components/navbar';
+import savart_logo from '@/assets/images/savart_logo.png';
+import Image from 'next/image';
 
 export default function Home() {
   return (
     <>
       <Navbar />
-      <main className="bg-hero bg-cover h-[100vh]"></main>
+      <main className="bg-hero bg-cover bg-center h-[100vh]">
+        <div>
+          <Image src={savart_logo} alt="savart logo" className="pt-24 ms-16 w-24 z-10" />
+          <h1 className="ms-16 mt-28 tracking-wide leading-relaxed lg:max-w-[40vw] tracking-2 text-4xl text-white font-extralight">
+            Skuter Masa Depan yang Melampaui Batas dan Ekspektasi
+          </h1>
+        </div>
+      </main>
       <main className="bg-hero bg-cover h-[100vh]"></main>
     </>
   );
